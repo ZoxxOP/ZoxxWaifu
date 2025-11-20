c55c8fb01f0cd4cf62d96df2316025364e81c540
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-11-20 16:46:28 +0530

    Delete ZoxxNetwork/modules/callback.py

--- ZoxxNetwork/modules/callback.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_callback_query(filters.regex("entertainment_video"))
-async def play_entertainment(_, query):
-    await query.answer()
-    await query.message.reply_video(
-        video="https://files.catbox.moe/m5qcx3.mp4"
-    )
